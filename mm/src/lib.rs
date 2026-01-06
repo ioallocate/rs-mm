d679045c80ea206539259cbdff796599bcbabb79
ioallocate <ioallocate@gmail.com>
2026-01-06 09:31:46 -0800

    New Feature, mouse move added

--- mm/src/lib.rs
+++ mm/src/lib.rs
@@ -3,8 +3,10 @@ pub mod module;
 pub mod handle;
 pub mod error;
 pub mod memory;
+pub mod mouse;
 
 pub use error::{mm_error, Result};
 pub use handle::{p_handle};
 pub use memory::{mmg};
 pub use module::ModuleInfo;
+pub use mouse::Mouse;
